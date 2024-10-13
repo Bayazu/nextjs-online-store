13edd6f7d0088c4fd92c26d3abf1ed39dcfd9156
Alexander Titov <a.titov@equiron.com>
2024-10-13 17:05:20 +0600

    add modal

--- app/(root)/@modal/(.)product/[id]/page.tsx
+++ app/(root)/@modal/(.)product/[id]/page.tsx
@@ -2,7 +2,7 @@ import { prisma } from '@/prisma/prisma-client';
 import { notFound } from 'next/navigation';
 import { ChooseProductModal } from '@/components/shared/modals';
 
-// TODO не использовать модальное окно, как отдельную страницу, это полное говнище, за исключением того, если после f5 нужно открыть новую страницу
+// не использовать модальное окно, как отдельную страницу, это полное говнище, за исключением того, если после f5 нужно открыть новую страницу
 export default async function ProductModalPage({ params: { id } }: { params: { id: string } }) {
   const product = await prisma.product.findFirst({
     where: {
